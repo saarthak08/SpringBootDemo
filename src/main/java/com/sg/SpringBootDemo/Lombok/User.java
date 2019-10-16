b35cab6d7f7fd8bfdb26549fbbb65575ab12106f
Saarthak <saarthakgupta08@gmail.com>
2019-10-16 17:05:55 +0530

    Added Project Files

---
+++ src/main/java/com/sg/SpringBootDemo/Lombok/User.java
@@ -0,0 +1,11 @@
+package com.sg.SpringBootDemo.Lombok;
+
+
+import lombok.Getter;
+
+@Getter
+public class gUser {
+    private String name;
+    private Long salary;
+    private Integer id;
+}
---
+++ src/main/java/com/sg/SpringBootDemo/Lombok/UserMain.java
@@ -0,0 +1,8 @@
+package com.sg.SpringBootDemo.Lombok;
+
+public class UserMain {
+    public static void main(String[] args){
+        User user=new User();
+
+    }
+}
